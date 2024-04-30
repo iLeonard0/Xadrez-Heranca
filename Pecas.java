f0bc670c572ee4fd203c987831251e03f6372f20
iLeonard0 <137785767+iLeonard0@users.noreply.github.com>
2024-04-30 19:56:23 -0300

    Update Pecas.java

--- Pecas.java
+++ Pecas.java
@@ -1,1 +1,17 @@
+package br.xadrez;
+
+public class Pecas {
+
+    protected static int x; // posição de cada peça
+    protected static int y; // posição de cada peça
+    protected boolean brancas; // cor da peça
+
+    public Pecas(int x, int y, boolean brancas){
+        this.x = x;
+        this.y = y;
+        this.brancas = brancas;
+    }
+
+    public abstract boolean MovimentoValido(int novoX, int novoy);
+}
 
