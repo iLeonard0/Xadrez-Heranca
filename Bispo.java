d4a77501c7d8f3e203807f9c2b25ecd352113849
iLeonard0 <137785767+iLeonard0@users.noreply.github.com>
2024-05-02 13:52:55 -0300

    Update Bispo.java

--- Bispo.java
+++ Bispo.java
@@ -1,9 +1,64 @@
 package br.xadrez;
+import java.util.List;
+import java.util.ArrayList;
+public class Bispo extends Pecas {
 
-public class Bispo extends Pecas{
-
-    public Bispo(int x, int y, boolean brancas) {
-        super(x, y, brancas);
+    public Bispo(String cor, int linha, int coluna) {
+        super(cor, linha, coluna);
     }
-}
 
+    private String MovimentoBispo;
+    private String CapturaBispo;
+    private String CorBispo;
+    private String LinhaBispo;
+    private String ColunaBispo;
+
+    public List<String> MovimentoBispo(){
+        List<String> movimentos = new ArrayList<>();
+        movimentos.add("Mover na diagonal em qualquer direção.");
+        movimentos.add("Capturar peças adversárias na diagonal e na casa em que parou de se movimentar.");
+        movimentos.add("Não pode pular sobre outras peças.");
+        return movimentos;
+    }
+
+    public void setCapturaBispo(String capturaBispo) {
+        CapturaBispo = capturaBispo;
+    }
+
+    public void setColunaBispo(String colunaBispo) {
+        ColunaBispo = colunaBispo;
+    }
+
+    public void setCorBispo(String corBispo) {
+        CorBispo = corBispo;
+    }
+
+    public void setLinhaBispo(String linhaBispo) {
+        LinhaBispo = linhaBispo;
+    }
+
+    public void setMovimentoBispo(String movimentoBispo) {
+        MovimentoBispo = movimentoBispo;
+    }
+
+    public String getCorBispo() {
+        return CorBispo;
+    }
+
+    public String getCapturaBispo() {
+        return CapturaBispo;
+    }
+
+    public String getColunaBispo() {
+        return ColunaBispo;
+    }
+
+    public String getLinhaBispo() {
+        return LinhaBispo;
+    }
+
+    public String getMovimentoBispo() {
+        return MovimentoBispo;
+    }
+
+}
